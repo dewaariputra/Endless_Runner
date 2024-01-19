fdd8a495be11183c8866cc56dce1ea726f1237db
dewaariputra <dewaarik20@gmail.com>
2024-01-19 13:25:29 +0800

    first commit

---
+++ Assets/Scripts/Collactables/CollacTableControl.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollacTableControl : MonoBehaviour
+{
+    public static int coinCount;
+    public GameObject coinCountDisplay;
+    public GameObject coinEndDisplay;
+    void Update()
+    {
+        coinCountDisplay.GetComponent<Text>().text = "" + coinCount;
+        coinEndDisplay.GetComponent<Text>().text = "" + coinCount;
+    }
+}
